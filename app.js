389d523a54b8bc2a647f01cc34275cab26e93f18
ghigbie <gehigbie@gmail.com>
2017-02-15 22:51:26 +0000

    added ejs file and is now working

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var app = express();
 
 //this is the root route
 app.get("/", function(req, res){
-    res.render("views/view.ejs");
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing.toLowerCase();
+    res.send("<h1>Fall in love with " + thing+ "."</h1>);
+    
 });
 
 //the first two arguments are specific to the cloud9 environment

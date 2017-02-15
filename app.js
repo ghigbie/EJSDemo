bf43cec78631123a9e135cec4e773cfefe7ed09b
ghigbie <gehigbie@gmail.com>
2017-02-15 21:14:53 +0000

    added app.get and app.listen

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var express = require("express");
 var app = express();
+
+//this is the root route
+app.get("/", function(req, res){
+    res.send("<h1>Welcome to the home page</h1>");
+});
+
+//the first two arguments are specific to the cloud9 environment
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is listening...Yay, yay!!!");
+});

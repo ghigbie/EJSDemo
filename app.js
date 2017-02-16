2548515b02a2968273a7b383961c3caac8ef5b5f
ghigbie <gehigbie@gmail.com>
2017-02-16 01:02:30 +0000

    added new app.get method with array data

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "Cats are awesome 2", author: "Beth"},
+        {title: "Cows are awesome 3", author: "Jill"},
+        {title: "Dogs are cool too 4", author: "Nat"},
+     ];
+     
+     res.render("posts.ejs", {posts: posts});
+});
+
 //the first two arguments are specific to the cloud9 environment
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening...Yay, yay!!!");

f0367a3ebb066b5d3d0e5fbeee6976000320a568
ghigbie <gehigbie@gmail.com>
2017-02-16 01:54:01 +0000

    added ejs engine

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 //this is the root route
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing.toLowerCase();
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -21,7 +22,11 @@ app.get("/posts", function(req, res){
         {title: "Dogs are cool too 4", author: "Liz"},
      ];
      
-     res.render("posts.ejs", {posts: posts});
+     res.render("posts", {posts: posts});
+});
+
+app.get("*", function(req, res){
+    res.send("<h1>This page is not here</h1>");
 });
 
 //the first two arguments are specific to the cloud9 environment
